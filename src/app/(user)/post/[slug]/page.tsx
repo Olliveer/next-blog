0065f52a47dd4132918653eb88f4e913b8bf84c8
olliveer <j.oliveira90@live.com>
2023-02-05 22:37:30 -0300

    feat: add revalidate static pages

---
+++ src/app/(user)/post/[slug]/page.tsx
@@ -0,0 +1,107 @@
+import { groq } from "next-sanity";
+import Image from "next/image";
+import { PortableText } from "@portabletext/react";
+import { client } from "../../../../../lib/sanity.client";
+import urlFor from "../../../../../lib/urlFor";
+import { richTextCompoents } from "../../../../components/RichTextCompoents";
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+  const query = groq`
+    *[_type == "post"] {
+      slug
+    }
+  `;
+
+  const slugs: Post[] = await client.fetch(query);
+
+  const slugRoutes = slugs.map((slug) => slug.slug.current);
+
+  return slugRoutes.map((slug) => ({
+    slug,
+  }));
+}
+
+async function Post({ params: { slug } }: Props) {
+  const query = groq`
+    *[_type == "post" && slug.current == $slug][0] {
+      ...,
+      author->,
+      categories[]->
+    }
+  `;
+
+  const post: Post = await client.fetch(query, { slug });
+
+  return (
+    <article className="px-10 pb-28">
+      <section className="space-y-2 border border-brand-500 text-white">
+        <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
+          <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
+            <Image
+              className="object-cover object-center mx-auto"
+              src={urlFor(post.mainImage).url()}
+              alt={post.author.name}
+              fill
+            />
+          </div>
+
+          <section className="p-5 bg-brand-500">
+            <div className=" flex flex-col md:flex-row justify-between gap-y-5">
+              <div className="">
+                <h1 className="text-4xl font-extrabold">{post.title}</h1>
+                <p className="text-sm">
+                  {Intl.DateTimeFormat("pt-BR", {
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric",
+                  }).format(new Date(post._createdAt))}
+                </p>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <Image
+                  className="rounded-full"
+                  src={urlFor(post.author.image).url()}
+                  alt={post.author.name}
+                  width={40}
+                  height={40}
+                />
+
+                <div className="w-64">
+                  <h3 className="text-lg font-bold">{post.author.name}</h3>
+                  <div className="">{/* TODO: Author bio **/}</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="">
+              <h2 className="italic pt-10">{post.description}</h2>
+              <div className="flex items-center justify-end mt-auto space-x-2">
+                {post.categories.map((category) => (
+                  <div
+                    key={category._id}
+                    className="bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4"
+                  >
+                    {category.title}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
+
+      <PortableText value={post.body} components={richTextCompoents} />
+    </article>
+  );
+}
+
+export default Post;
